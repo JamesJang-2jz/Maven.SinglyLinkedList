717a43e28f2aae9751bbd7b0c8716ab874582e38
James <james@Zipcoders-MacBook-Pro-101.local>
2023-04-01 21:51:01 -0400

    changing to generics, cant pass Node value into parameter

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,59 @@
 package com.zipcodewilmington.singlylinkedlist;
-
+import jdk.nashorn.internal.ir.Node;
+import java.util.LinkedList;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+    static class Node<T>{ // making static Node class within LinkedList class
+        T data;
+        Node<T> next;
+//        Node prev;
+        public Node(T d){
+            data = d;
+            next = null;
+        }
+    }
+    public SinglyLinkedList() {
+        head = null;
+        tail = null;
+    }
+    /**
+     * add: If head/first node doesn't exist, head equals i
+     * else if a head exists, the tail.next to next node will point to i
+     * tail will also equal i
+     */
+    public void add(int i){
+        if (head == null){
+            head = i;
+            tail = i;
+        } else {
+            tail.next = i;
+            tail = i;
+        }
+    }
+    /**
+     *
+     */
+    public void remove(Node j) {
+        Node temp;
+        Node n = head;
+        if (n != null && head == j) {
+            head = n.next;
+        }
+        while (n != null && n.data != j) {
+            if (n.next == j) {
+                temp = j.next;
+
+            }
+        }
+    }
+
+    // todo: add, remove, contains, find, size, get, copy and sort methods
+
+
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,30 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import jdk.nashorn.internal.ir.Node;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest{
+    @Test
+    public void testAdd(){
+        // Create some nodes
+//        SinglyLinkedList sing = new SinglyLinkedList();
+
+        // Add the nodes to the list
+//        sing.add();
+        // Check that the head is correct
+//        Assert.assertEquals(expected, sing.head);
+
+        // Check that the tail is correct
+
+        // Check that the nodes are linked correctly
+//        assertEquals(node2, node1.next);
+//        assertNull(node3.next);
+    }
+
 }
